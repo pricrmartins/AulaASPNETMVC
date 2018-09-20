dadccdd56628f548c2dea17597f603972569d65c
FEESC\priscilla.martins <priscilla@feesc.org.br>
2018-09-20 08:58:52 -0300

    Primeiro Controller

---
+++ AulaASPNETMVC/Controllers/PrimeiroController.cs
@@ -0,0 +1,19 @@
+﻿using AulaASPNETMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AulaASPNETMVC.Controllers
+{
+    public class PrimeiroController : Controller
+    {
+        // GET: Primeiro
+        public ActionResult Index()
+        {
+            PrimeiroModel primeiroModel = new PrimeiroModel { Nome="Priscilla" };
+            return View(primeiroModel);
+        }
+    }
+}
---
+++ AulaASPNETMVC/Models/PrimeiroModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AulaASPNETMVC.Models
+{
+    public class PrimeiroModel
+    {
+        public string Nome { get; set; }
+    }
+}
